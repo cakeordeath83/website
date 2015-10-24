15fe04d1701696ad47eb15c05c5a1879d4adf062
cakeordeath83 <clair_griffiths@hotmail.com>
2015-10-24 19:25:36 +0000

    Hopefully fixed 404 image

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module Website
     # Do not swallow errors in after_commit/after_rollback callbacks.
 		config.exceptions_app = self.routes
     config.active_record.raise_in_transactional_callbacks = true
+		
   end
 end
