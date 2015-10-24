2dde9e366ff2a1f06220d3c8e6ae557f9596b87a
cakeordeath83 <clair_griffiths@hotmail.com>
2015-10-24 11:50:03 +0000

    Added breadcrumb to tag

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,10 @@
 class TagsController < ApplicationController
 	
+	add_breadcrumb "LEARNING GUIDES", :snippets_path
 	
 	def show
+		
 		@tag = Tag.find(params[:id])
+		add_breadcrumb "#{@tag.name.upcase}"
 	end
 end
