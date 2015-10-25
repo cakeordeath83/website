b3afe60cd6343b6ee2cf7984e080678ff2a48c68
cakeordeath83 <clair_griffiths@hotmail.com>
2015-10-25 09:22:49 +0000

    Added category to tags and associated views

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,10 +1,38 @@
 class TagsController < ApplicationController
 	
+	before_action :find_tag, only: [:show, :edit, :update]
 	add_breadcrumb "LEARNING GUIDES", :snippets_path
 	
 	def show
-		
-		@tag = Tag.find(params[:id])
 		add_breadcrumb "#{@tag.name.upcase}"
 	end
+	
+	def new
+	end
+	
+	def index
+		@tags = Tag.all
+	end
+	
+	def edit
+	end
+	
+	def update
+		if @tag.update(tag_params)
+			redirect_to tags_path
+		else
+			render :edit
+		end
+	end
+	
+	private
+	
+	def tag_params
+		params.require(:tag).permit(:name, :category)
+	end
+	
+	def find_tag
+		@tag = Tag.find(params[:id])
+	end
+	
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151024095549) do
+ActiveRecord::Schema.define(version: 20151025090328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20151024095549) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "category"
   end
 
   create_table "users", force: :cascade do |t|
