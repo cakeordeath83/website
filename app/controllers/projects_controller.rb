db93946f7d1a90eae79abedb5916cbd23d9f1180
cakeordeath83 <clair_griffiths@hotmail.com>
2016-02-01 17:21:58 +0000

    Tidied up project page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,7 @@
 class ProjectsController < ApplicationController
   
+	before_action :find_project, only: [:edit, :update, :delete]
+	
   def index
     @projects = Project.all
   end
@@ -16,10 +18,25 @@ class ProjectsController < ApplicationController
       render :new
     end
   end
+	
+	def edit
+	end
+	
+	def update
+		if @project.update(project_params)
+			redirect_to projects_path
+		else
+			render :edit
+		end
+	end
   
 private
   def project_params
     params.require(:project).permit(:title, :link, :image)
   end
+	
+	def find_project
+		@project = Project.find(params[:id])
+	end
   
 end
