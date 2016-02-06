f3018d03ea406b57541ef30d08e945ba82a9add1
cakeordeath83 <clair_griffiths@hotmail.com>
2016-02-06 16:03:07 +0000

    Images now working

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -4,14 +4,9 @@ module PostsHelper
 		@body.group_by{|entry| entry["start"].to_date}
 	end
 	
-	def find_project(pid)
-		@projecturl = "https://www.toggl.com/api/v8/projects/#{pid}"
-		request = HTTParty.get(@projecturl, {basic_auth: {username: 'f89f3fac7a437c624ec0964143b62c02', password: 'api_token'}})
-		@projectbody = JSON.parse(request.body)
-		@projectbody["data"]["name"]
-	end
+	
 	
 	def live_post(time_entries)
-		time_entries.last["stop"].nil? ? time_entries.last : nil
+		time_entries.last.stop.nil? ? time_entries.last : nil
 	end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -6,8 +6,9 @@ class Post
 		 week_ago = Date.today - 7
 		 now = "#{Date.today.iso8601}T#{Time.now.strftime("%H")}%3A#{Time.now.strftime("%M")}%3A#{Time.now.strftime("%S")}%2B00%3A00"
      last_week = "#{week_ago.iso8601}T00%3A00%3A00%2B00%3A00"
+     api = ENV["TOGGL_API_USERNAME"]
 		 @url = "https://toggl.com/api/v8/time_entries?start_date=#{last_week}&end_date=#{now}"
-		 request = HTTParty.get(@url, {basic_auth: {username: "f89f3fac7a437c624ec0964143b62c02", password: 'api_token'}})
+		 request = HTTParty.get(@url, {basic_auth: {username: "#{api}", password: 'api_token'}})
 		 @body = JSON.parse(request.body)
 # Since it isn't going into the database, it doesn't need to touch the contoller
 # .each would work but more code than needed
@@ -21,15 +22,20 @@ class Post
 			end
   end
 	
-	def initialize(params)
-		self.params = params
-	end
+  def initialize(params)
+    self.params = params
+  end
+
+  def description
+    params["description"]
+  end
 
   def stop
-		params["stop"]
-	end
-	
-	def finished?
-		params['stop'].nil?
-	end
+    params["stop"]
+  end
+
+
+  def finished?
+    params['stop'].nil?
+  end
 end
