b1b4a5963e83d4b7d19c50382109c0dc823238ab
cakeordeath83 <clair_griffiths@hotmail.com>
2016-05-02 15:16:50 +0000

    Added Dashboard route and page

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -3,9 +3,11 @@ require 'toggl_api'
 class TimeEntriesController < ApplicationController
   def index
     @time_entries = TimeEntry.all
-
     to = Time.zone.now
     from = Time.zone.now.beginning_of_day
     @today_time_entries = TogglApi.get_time_entries(from: from, to: to)
+  end
+
+  def dashboard
   end
 end
