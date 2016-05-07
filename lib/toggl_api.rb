b3a17472c7d6b9e5457cf92e6f3f051374a555b4
cakeordeath83 <clair_griffiths@hotmail.com>
2016-05-07 11:23:30 +0000

    Centered dashboard buttons at top of page

--- lib/toggl_api.rb
+++ lib/toggl_api.rb
@@ -13,7 +13,6 @@ module TogglApi
   def self.get_dashboard_info(from:, to:)
     api = ENV["TOGGL_API_USERNAME"]
     url = "https://toggl.com/reports/api/v2/summary?workspace_id=543572&since=#{from}&until=#{to}&user_agent=api_test"
-    
     request = HTTParty.get(url, {basic_auth: {username: "#{api}", password: 'api_token'}})
     JSON.parse(request.body)
   end
